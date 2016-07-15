a22e725b82487d957d2310ad26edc528186b0c94
ishida <ishida@psid.co.jp>
2016-07-15 14:04:31 +0900

    [FIX] 上へ戻るアイコンの実装

--- js/topscroll.js
+++ js/topscroll.js
@@ -1,29 +1,27 @@
 // JavaScript Document
-    //上へ戻るスクリプト
-    <script type="text/javascript">
-        $(function() {
-            var showFlag = false;
-            var topBtn = $('#page-top');
-            topBtn.css('bottom', '-100px');
-            var showFlag = false;
-            $(window).scroll(function () {
-                if ($(this).scrollTop() > -200) {
-                    if (showFlag == false) {
-                        showFlag = true;
-                        topBtn.stop().animate({'bottom' : '20px'}, 200);
-                    }
-                } else {
-                    if (showFlag) {
-                        showFlag = false;
-                        topBtn.stop().animate({'bottom' : '-100px'}, 200);
-                    }
-                }
-            });
-            topBtn.click(function () {
-                $('body,html').animate({
-                    scrollTop: 0
-                }, 500);
-                return false;
-            });
-        });
-    </script>
+//上へ戻るスクリプト
+$(function() {
+    var showFlag = false;
+    var topBtn = $('#page-top');
+    topBtn.css('bottom', '-100px');
+    var showFlag = false;
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > -200) {
+            if (showFlag == false) {
+                showFlag = true;
+                topBtn.stop().animate({'bottom' : '20px'}, 200);
+            }
+        } else {
+            if (showFlag) {
+                showFlag = false;
+                topBtn.stop().animate({'bottom' : '-100px'}, 200);
+            }
+        }
+    });
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
